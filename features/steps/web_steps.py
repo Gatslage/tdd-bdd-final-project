e519918d24b327f4e85520d1d7fe3d8ccfd477dc
Gamtchessi Teubo Apollos <95596324+Gatslage@users.noreply.github.com>
2024-04-07 06:35:32 +0100

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -111,28 +111,22 @@ def step_impl(context, button):
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
-    found = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.text_to_be_present_in_element(
-            (By.ID, 'search_results'),
-            name
-        )
+    fTry = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element((By.ID, 'search_results'),name)
     )
-    assert(found)
+    assert(fTry)
 
-@then('I should not see "{name}" in the results')
-def step_impl(context, name):
-    element = context.driver.find_element_by_id('search_results')
-    assert name not in element.text
+@then('I should not see "{elt_prod}" in the results')
+def step_impl(context, elt_prod):
+    element_r = context.driver.find_element(By.ID,'search_results')
+    assert elt_prod not in element_r.text
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
-    found = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.text_to_be_present_in_element(
-            (By.ID, 'flash_message'),
-            message
-        )
+    notification = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element((By.ID, 'flash_message'),message )
     )
-    assert found
+    assert notification
 
 
 ##################################################################
